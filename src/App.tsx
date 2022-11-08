5a773d1e30996445f1f040c7e92688ec40b2516b
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-11-07 20:42:10 -0800

    Get data Get users' data from JASON placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
 
 const user = {
   id: 1,
@@ -9,9 +12,27 @@ const user = {
 };
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([]);
+
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
+  };
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>Get Data</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
